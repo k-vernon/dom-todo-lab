9c20f88b905da4f4b434fb1e72964e510d4fe84a
k-vernon <kvernon908@gmail.com>
2022-12-07 23:04:03 -0500

    complete to-do list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log("sanity")
+
+const inputElement = document.getElementById("input")
+console.log(inputElement)
+const buttonElement = document.getElementById("submit-button")
+console.log(buttonElement)
+const ulElement = document.getElementById("todo-list")
+console.log(ulElement)
+
+buttonElement.addEventListener("click", function(){
+    const li = document.createElement("li")
+    console.log(li)
+    li.textContent = inputElement.value
+    ulElement.appendChild(li)
+    inputElement.value = ""
+})
+
+
+
+
+
